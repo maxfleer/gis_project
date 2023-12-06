f6a7ee55855e3b328fed650fbfd542b5d53fc5a9
braudantoine <antoine56.braud@gmail.com>
2023-12-06 14:49:28 +0100

    commit-antoine-1

--- gis_project/project/views.py
+++ gis_project/project/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import folium
 
 # Create your views here.
-
 def index (request):
-    return HttpResponse("1 HTML please")
+    m = folium.Map([52.408421, 12.562490], zoom_start=12)
+    folium.TileLayer('stamenterrain').add_to(m)
+
+    folium.Marker(
+        location=[52.408421, 12.562490],
+        tooltip="Click me!",
+        popup="<button>Hello</button>Brandenburg an der Havel",
+        icon=folium.Icon(icon="cloud"),
+    ).add_to(m)
+
+    context = {'map': m._repr_html_()}
+    return render(request,"index.html", context)
 
